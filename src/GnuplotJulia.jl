2db25eef8bdc861ea40eac44314fcbca0f3f136e
Enrique Gómez Cruz <enriquegomezcruz@gmail.com>
2018-11-22 14:12:43 -0600

    added plot macro

--- src/GnuplotJulia.jl
+++ src/GnuplotJulia.jl
@@ -1,12 +1,7 @@
 module GnuplotJulia
 
-export Gnuplot, epslatex!, png!, config!, tex2pdf, tex2png,
-       plot, mpeg, cleanup
-
-const Data2D = NTuple{2, Vector{<:Real}}
-const Data3D = NTuple{3, Vector{<:Real}}
-const Points2D = Vector{NTuple{2, <:Real}}
-const Points3D = Vector{NTuple{3, <:Real}}
+export Gnuplot, tex!, png!, openPlot!, closePlot!, cmd!, plot!, tex2pdf,
+tex2png, mpeg, cleanup
 
 mutable struct Gnuplot
     p::Base.Process
@@ -14,86 +9,86 @@ mutable struct Gnuplot
     padding::Int
     ext::String
     function Gnuplot()
-        p = open(`gnuplot`, "w")
-        print(p, "set colorsequence podo;")
-        return new(p, 0, 3)
+        gp = new()
+        gp.p = open(`gnuplot -p`, "w")
+        print(gp.p, "set colorsequence podo;")
+        gp.counter = 0 # File numbering. Useful when animating.
+        gp.padding = 3 # File name padding, e.g., 009, 010.
+        return gp
     end
 end
 
-function epslatex!(gp::Gnuplot, x::Int, y::Int)
+function tex!(x::Int, y::Int; opts::String = "")
     gp.ext = ".tex"
-    print(gp.p, "
-          set terminal epslatex standalone size $(x)cm, $(y)cm\\
-          color background 'white'\\
-          header '\\usepackage[utf8]{inputenc} \\usepackage{amsmath}';
-          ")
+    println(gp.p, "
+            reset;
+            set terminal epslatex standalone size $(x)cm, $(y)cm\\
+            color background 'white'\\
+            header '\\usepackage[utf8]{inputenc} \\usepackage{amsmath}';
+            set colorsequence podo;
+            ", opts)
 end
 
-function png!(gp::Gnuplot, x::Int, y::Int)
+function png!(x::Int, y::Int; opts::String = "")
     gp.ext = ".png"
-    print(gp.p, "
-          set terminal png enhanced nocrop font 'verdana, 8' size $x, $y;
-          ")
+    println(gp.p, "
+            reset;
+            set terminal png enhanced nocrop font 'verdana, 8' size $x, $y;
+            set colorsequence podo;
+            ", opts)
 end
 
-function config!(gp::Gnuplot, opts::String)
+macro plot(ex::Expr)
+    quote
+        println(gp.p, "set output 'tmp-",
+                lpad(gp.counter, gp.padding, '0'), gp.ext, "'")
+        $(esc(ex))
+        println(gp.p, "e")
+        gp.counter += 1
+    end
+end
+
+@inline function cmd!(opts::String)
     println(gp.p, opts)
 end
 
-function plot(gp::Gnuplot, data::Data2D, opts::String)
-    x::Vector{<:Real} = data[1]
-    y::Vector{<:Real} = data[2]
-    n::Int = min(length(x), length(y))
-
-    println(gp.p, "set output 'tmp-",
-            lpad(gp.counter, gp.padding, '0'), gp.ext, "'")
-    println(gp.p, "plot '-' ", opts)
-    @inbounds for i = 1:n
-        println(gp.p, x[i], " ", y[i])
-    end
-    println(gp.p, "e")
-    gp.counter += 1
+@inline function pts!(x::T, y::T) where {T <: Real}
+    println(gp.p, x, " ", y)
 end
 
-function plot(gp::Gnuplot, pts::Points2D, opts::String)
-    println(gp.p, "set output 'tmp-",
-            lpad(gp.counter, gp.padding, '0'), gp.ext, "'")
-    println(gp.p, "plot '-' ", opts)
-    @inbounds for i = 1:length(pts)
-        p::NTuple{2, <:Real}
-        println(gp.p, p[1], " ", p[2])
-    end
-    println(gp.p, "e")
-    gp.counter += 1
+@inline function pts!(x::T, y::T, z::T) where {T <: Real}
+    println(gp.p, x, " ", y, " ", z)
 end
 
-function tex2pdf(gp::Gnuplot)
+function tex2pdf()
     println(gp.p, "set output")
-    run(pipeline(`latexmk`, stdout=devnull, stderr=devnull))
+    run(pipeline(`latexmk`, stdout=devnull))
     for i = 0:gp.counter-1
         x::String = lpad(i, gp.padding, '0')
-        run(pipeline(`dvips tmp-$x.dvi`, stdout=devnull, stderr=devnull))
+        run(pipeline(`dvips tmp-$x.dvi`, stdout=devnull))
         run(pipeline(`gs -dBATCH -dSAFER -dNOPAUSE
                      -sDEVICE=pdfwrite -sOutputFile=output-$x.pdf
-                     tmp-$x.ps`, stdout=devnull, stderr=devnull))
+                     tmp-$x.ps`, stdout=devnull))
     end
+    run(`find -x . -name tmp-\* -delete`)
     #display("application/pdf", read("output.pdf"))
 end
 
-function tex2png(gp::Gnuplot)
+function tex2png()
     println(gp.p, "set output")
-    run(pipeline(`latexmk`, stdout=devnull, stderr=devnull))
+    run(pipeline(`latexmk`, stdout=devnull))
     for i = 0:gp.counter-1
         x::String = lpad(i, gp.padding, '0')
-        run(pipeline(`dvips tmp-$x.dvi`, stdout=devnull, stderr=devnull))
+        run(pipeline(`dvips tmp-$x.dvi`, stdout=devnull))
         run(pipeline(`gs -dBATCH -dSAFER -dNOPAUSE
                      -sDEVICE=pngalpha -sOutputFile=output-$x.png
-                     tmp-$x.ps`, stdout=devnull, stderr=devnull))
+                     tmp-$x.ps`, stdout=devnull))
     end
+    run(`find -x . -name tmp-\* -delete`)
     #display("image/png", read("output.png"))
 end
 
-function mpeg(gp::Gnuplot)
+function mpeg()
     run(pipeline(`ffmpeg -y -i output-%0$(gp.padding)d.png output.mpeg`,
                  stdout=devnull, stderr=devnull))
     #display("text/html", """
@@ -103,11 +98,12 @@ function mpeg(gp::Gnuplot)
     #        """)
 end
 
-function cleanup(gp::Gnuplot)
+function cleanup()
     close(gp.p)
     wait(gp.p.closenotify)
     gp.counter = 0
-    run(`find -x . -name tmp-\* -delete`)
 end
+
+global gp = Gnuplot()
 
 end # module
